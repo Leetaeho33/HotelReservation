cbf607bba083c26c6dffb426b7f9df62298da6c1
juneyoung Lee <junyeong237@gmail.com>
2023-10-24 21:11:21 +0900

    View 초기세팅

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("태호님 팀장 개꿀 개최고 개멋짐");
+
+
+
+        Scanner sc = new Scanner(System.in);
+        View consoleView = new View();
+
+        while(true) {
+            //시작 화면
+            consoleView.StartView();//예약, 예약조회,호텔관리 체크하는 분기
+            int checkCustom = sc.nextInt();
+            Customer customer = new Customer();
+            Hotel hotel = new Hotel();
+            switch (checkCustom) {
+
+                case 1:
+                    customer = consoleView.Custominfo(); // 고객정보를 입력받는 view
+                    consoleView.EmptyRoomView(hotel, customer);
+                    System.out.println("예약 기능 작동");
+                    break;
+
+                case 2: // 예약수정 및 조회
+                    consoleView.searchReservationView(/*uuid*/);
+                    break;
+
+
+                case 3: // 호텔관리
+                    break;
+
+
+                default:
+                    System.out.println("해당하지않는 번호입니다.");
+                    break;
+
+
+            }
+        }
     }
 }
