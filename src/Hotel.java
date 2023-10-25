c588e375e572a373f8d67e2f689dc8c930a17316
hwang <ghkdrbwjd369@naver.com>
2023-10-25 17:13:31 +0900

    checkReservation Update

--- src/Hotel.java
+++ src/Hotel.java
@@ -11,5 +11,29 @@ public class Hotel {
     public void addRoom(Room room){
         this.roomList.add(room);
     }
+
+    public void checkReservationCustomer(String uuId){
+        for(Map.Entry<String, Reservation> entry : reservationMap.entrySet()){
+            if(entry.getKey().equals(uuId)){
+                System.out.println("Name : " + entry.getValue().getCustomer().getCustomerName() + " | " +
+                        "Phone Number : " + entry.getValue().getCustomer().getCustomerPhoneNumber() + " | " +
+                        "Room Number : " + entry.getValue().getRoom().getRoomNumber() + " | " +
+                        "Reservation Date : " + entry.getValue().reservationDate + " | " +
+                        "UUID : " + entry.getKey());
+            }
+        }
+    }
+
+    public void checkReservationHotel(){
+        for (Map.Entry<String, Reservation> entry : reservationMap.entrySet()) {
+            System.out.println("Name : " + entry.getValue().getCustomer().getCustomerName() + " | " +
+                    "Phone Number : " + entry.getValue().getCustomer().getCustomerPhoneNumber() + " | " +
+                    "Room Number : " + entry.getValue().getRoom().getRoomNumber() + " | " +
+                    "Reservation Date : " + entry.getValue().reservationDate + " | " +
+                    "UUID : " + entry.getKey());
+        }
+    }
+
+
 }
 
--- src/Reservation.java
+++ src/Reservation.java
@@ -1,12 +1,12 @@
+import java.time.LocalDate;
 import java.util.*;
 
 public class Reservation {
     private Customer customer;
     private Room room;
-    Date reservationDate; // 이게 리스트가 아니어도 상관없지 않을까요...?
-    Reservation reservation;
+    LocalDate reservationDate; // 이게 리스트가 아니어도 상관없지 않을까요...?
 
-    public Reservation makeReservation(Customer customer, Room room, Date date){
+    public Reservation makeReservation(Customer customer, Room room, String date){ //
         this.customer = customer;                                          //예약 객체의 고객 정보 저장
         this.room = room;                                                  //예약 객체의 방 정보 저장
         if(this.customer.getBudget() > this.room.getRoomPrice()){          //손님의 예산 > 방가격
@@ -14,30 +14,19 @@ public class Reservation {
 //                                                                              반복해서 받는작업이 있어서.. 회원가입을 만들지 않는다면 소지금을 빼줄 필요가 없을것같습니다..!
             UUID uuid = UUID.randomUUID();                                 //고객에게 제공할 UUID 생성
             customer.addUuid(uuid.toString());                             //예약 성공 시 고객에게 UUID 제공
+            reservationDate = LocalDate.parse(date);
         }
-        return reservation;                                                //예약 성공 시 예약 객체 반환
+        return ;                                                //예약 성공 시 예약 객체 반환
     }                                                                      //예약 객체는 고객, 방, 날짜 정보 포함
 
-    public void CheckReservationCustomer(Map<String, Reservation> reservationMap,String uuId){
-        for(Map.Entry<String, Reservation> entry : reservationMap.entrySet()){
-            if(entry.getKey().equals(uuId)){
-                System.out.println("Name : " + entry.getValue().customer.getCustomerName() +
-                        "Phone Number : " + entry.getValue().customer.getCustomerPhoneNumber() +
-                        "Room Number : " + entry.getValue().room.getRoomNumber() +
-                        "Reservation Date : " + entry.getValue().reservationDate +
-                        "UUID : " + entry.getKey());
-            }
-        }
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void CheckReservationHotel(Map<String, Reservation> reservationMap){
-        for (Map.Entry<String, Reservation> entry : reservationMap.entrySet()) {
-            System.out.println("Name : " + entry.getValue().customer.getCustomerName() +
-                    "Phone Number : " + entry.getValue().customer.getCustomerPhoneNumber() +
-                    "Room Number : " + entry.getValue().room.getRoomNumber() +
-                    "Reservation Date : " + entry.getValue().reservationDate +
-                    "UUID : " + entry.getKey());
-        }
+    public Room getRoom() {
+        return room;
     }
+
+
 }
 // 예약 // 예약 취소 // 예약 확인 전체 예약확인
