bfec1df002f6b09bb11b15fe796a7cbcec702439
taeholee <lth1993@naver.com>
2023-10-26 14:56:31 +0900

    customer class, room class, reservation class

--- src/Room.java
+++ src/Room.java
@@ -1,37 +1,35 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Room {
 
     private int roomNumber; // 각 방의 갯수는 3개
     private String roomSize; // 1층 Standard 2층 Twin 3층 Deluxe 4층 Suite
     private int roomPrice; // standard 5만원, Twin 10만원, Deluxe 15만원, Suite 40만원
+    private ArrayList<Date> roomDateList = new ArrayList<>(); // 방의 날짜별 예약을 위한 날짜 list
 
     public int getRoomNumber() {
         return roomNumber;
     }
-
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
     public String getRoomSize() {
         return roomSize;
     }
-
-    public void setRoomSize(String roomSize) {
-        this.roomSize = roomSize;
-    }
-
     public int getRoomPrice() {
         return roomPrice;
     }
-
-    public void setRoomPrice(int roomPrice) {
-        this.roomPrice = roomPrice;
-    }
-
     public Room(int roomNumber, String roomSize, int roomPrice) {
         this.roomNumber = roomNumber;
         this.roomSize = roomSize;
         this.roomPrice = roomPrice;
     }
-
+    public boolean checkRoomReservation(Date date){         //룸 예약 가능 확인 메소드
+        if(roomDateList.contains(date)){                    //룸의 date list에 매개변수 date값이있냐?
+            return false;                                   //있으면 예약 불가능!
+        }else {
+            return true;                                    //없으면 예약 가능
+        }
+    }
+    public void addRoomDate(Date date){
+        roomDateList.add(date);
+    }
 }
